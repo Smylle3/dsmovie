e923913feed19d76f84f1470b995d6c650855dbc
Smylle3 <joao21731@gmail.com>
2022-01-10 22:04:07 -0300

    NavBar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as GitIcon} from 'assets/img/Vector.svg';
+import './styles.css';
+
+function Navbar() {
+
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+            <a href="https://github.com/Smylle3" className="teimoso" target="_blank" rel="noreferrer">
+              <div className="dsmovie-contact-container">
+                <GitIcon />
+                <p className="dsmovie-contact-link">/Smylle3</p>
+              </div>
+            </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
