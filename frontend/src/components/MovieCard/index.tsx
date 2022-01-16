7eae4241cfe88c95dbadc9cd996dbfeb17757e41
Smylle3 <joao21731@gmail.com>
2022-01-16 13:33:20 -0300

    useParams

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -11,13 +11,11 @@ function MovieCard( { movie } : Props) {
     return (
         <div className="background">
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <a href="/form/1" className="dsmovie-card-name">
                 <div className="dsmovie-card-bottom-container">
                     <h3>{movie.title}</h3>
                     <MovieScore />
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </div>
-            </a>
         </div >
     );
 }
