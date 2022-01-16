7c0211093b6f1dcec55cf3179950fbe1a302551e
Smylle3 <joao21731@gmail.com>
2022-01-16 15:17:14 -0300

    Save, useNavigate

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -3,19 +3,19 @@ import { Movie } from "types/movie";
 import "./styles.css";
 
 type Props = {
-    movie:Movie;
+    movie: Movie;
 }
 
-function MovieCard( { movie } : Props) {
+function MovieCard({ movie }: Props) {
 
     return (
         <div className="background">
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-                <div className="dsmovie-card-bottom-container">
-                    <h3>{movie.title}</h3>
-                    <MovieScore count={movie.count} score={movie.score} />
-                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-                </div>
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <MovieScore count={movie.count} score={movie.score} />
+                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+            </div>
         </div >
     );
 }
