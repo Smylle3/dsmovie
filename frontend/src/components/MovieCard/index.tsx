9e57ef651622fe2f4a8a9cc6baf845cbe8d2808d
Smylle3 <joao21731@gmail.com>
2022-01-15 21:39:41 -0300

    Fix more bugs

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -14,12 +14,14 @@ function MovieCard() {
     return (
         <div className="background">
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsmovie-card-bottom-container">
-                <h3><a href="/form/1" className="dsmovie-card-name">{movie.title}</a></h3>
-                <MovieScore />
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-            </div>
-        </div>
+            <a href="/form/1" className="dsmovie-card-name">
+                <div className="dsmovie-card-bottom-container">
+                    <h3>{movie.title}</h3>
+                    <MovieScore />
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </div>
+            </a>
+        </div >
     );
 }
 
