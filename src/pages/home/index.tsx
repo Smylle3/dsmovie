1ba211c93c2f4af19300365e2987730f6227f159
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-16 13:15:37 -0300

    Cria a página home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import NavBar from 'components/navbar'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1 >Página inicial</h1>
+    </div>
+  )
+}
