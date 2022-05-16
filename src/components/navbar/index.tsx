c8cb93d11d6e7d458d163710efc363de841e5dab
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-16 13:15:16 -0300

    Cria componente navBar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+import { SiGithub } from 'react-icons/si';
+
+export default function NavBar() {
+    return (
+        <header>
+            <div className='navbar'>
+                <h1>DSMovie</h1>
+                <a target='_blank' href='https://github.com/Smylle3/dsmovie' rel="noreferrer">
+                    <SiGithub className='icon' size={25} /> /Smylle3
+                </a>
+            </div>
+        </header>
+    )
+}
