a0f1dc74b2090a14d972b2c9e3df9066aea52c58
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-17 14:34:12 -0300

    Ativa opçoes de direcionamento

--- src/components/cardMovie/index.tsx
+++ src/components/cardMovie/index.tsx
@@ -1,19 +1,20 @@
 import StarHating from 'components/starHating';
 import React from 'react'
-/* import { Link } from 'react-router-dom' */
+import { Link } from 'react-router-dom'
 import './styles.css'
 
 export default function CardMovie(props: any) {
+  console.log(props.name)
   return (
     <div className='cardMovie'>
-      <img alt='MovieImg' src={props.name.Poster} className='poster' />
-      <h1>{props.name.Title}</h1>
+      <img alt='MovieImg' src={props.name.poster_path} className='poster' />
+      <h1 className='title'>{props.name.title}</h1>
 
-      <StarHating hate={props.name.imdbRating * 0.5} />
+      <StarHating hate={props.name.vote_average * 0.5} />
 
-      {/* <Link to='/form' className='details' >
+      <Link to='/form' className='details' >
         DETALHES
-      </Link> */}
+      </Link>
     </div>
   )
 }
