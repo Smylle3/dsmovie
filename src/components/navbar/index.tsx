ed16a94031b5dbc2fdc1be4757839be185377f15
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-17 00:52:51 -0300

    Adiciona icone ao título

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import './styles.css'
 import { SiGithub } from 'react-icons/si';
 import { Link } from 'react-router-dom';
+import { MdMovie } from 'react-icons/md';
 
 export default function NavBar() {
     return (
         <>
             <header>
                 <div className='navbar'>
-                    <Link to='/' ><h1>DSMovie</h1></Link>
+                    <Link to='/' className='title'>
+                        <MdMovie className='titleIcon' size={40} />
+                        <h1>DSMovie</h1>
+                    </Link>
                     <text className='info' >
                         © 2022 Smylle, development by React Js. All rights reserved
                     </text>
