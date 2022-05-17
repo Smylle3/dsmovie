368e0573e1ff112e281a2d5219a96fde29e90bcf
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-17 00:42:09 -0300

    Torna a header fixa e adiciona infos

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import './styles.css'
 import { SiGithub } from 'react-icons/si';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
-        <header>
-            <div className='navbar'>
-                <h1>DSMovie</h1>
-                <a target='_blank' href='https://github.com/Smylle3/dsmovie' rel="noreferrer">
-                    <SiGithub className='icon' size={25} />
-                    /Smylle3
-                </a>
-            </div>
-        </header>
+        <>
+            <header>
+                <div className='navbar'>
+                    <Link to='/' ><h1>DSMovie</h1></Link>
+                    <text className='info' >
+                        © 2022 Smylle, development by React Js. All rights reserved
+                    </text>
+                    <a target='_blank' href='https://github.com/Smylle3/dsmovie' rel="noreferrer">
+                        <SiGithub className='icon' size={25} />
+                        /Smylle3
+                    </a>
+                </div>
+            </header>
+            <div className='fix' />
+        </>
     )
 }
