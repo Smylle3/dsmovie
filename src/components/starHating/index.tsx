77c8fc6e0f732603f7fb512df6f83d88fb131b11
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-17 00:44:17 -0300

    Cria configuração de hating  de estrelas

---
+++ src/components/starHating/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { BsStar, BsStarFill } from "react-icons/bs";
+
+export default function StarHating(props: any) {
+    return (
+        <div className='hate' >
+            <div className='hating' >
+                {props.hate >= 1 ?
+                    <BsStarFill size={25} className='hating' />
+                    : <BsStar size={25} className='hating' />
+                }
+            </div>
+            <div className='hating' >
+                {props.hate >= 2 ?
+                    <BsStarFill size={25} className='hating' />
+                    : <BsStar size={25} className='hating' />
+                }
+            </div>
+            <div className='hating' >
+                {props.hate >= 3 ?
+                    <BsStarFill size={25} className='hating' />
+                    : <BsStar size={25} className='hating' />
+                }
+            </div>
+            <div className='hating' >
+                {props.hate >= 4 ?
+                    <BsStarFill size={25} className='hating' />
+                    : <BsStar size={25} className='hating' />
+                }
+            </div>
+            <div className='hating' >
+                {props.hate === 5 ?
+                    <BsStarFill size={25} className='hating' />
+                    : <BsStar size={25} className='hating' />
+                }
+            </div>
+        </div>
+    )
+}
